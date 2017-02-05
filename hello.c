da73afeba8d0c34486e62338817cfcf6a0e0e684
jeremyyang824 <jeremyyang824@aliyun.com>
2017-02-05 17:28:00 +0800

    test

--- hello.c
+++ hello.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_COLS 20     /*所能处理的最大列号*/
+#define MAX_INPUT 1000  /*每个输入行的最大长度*/
 
-int main()
+int read_column_numbers(int columns[], int max);
+void rearrange(char *output, char const *input);
+
+int main(void)
 {
-    printf("Hello World\n");
-    return 0;
+    return EXIT_SUCCESS;
 }
